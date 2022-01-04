44c78496ccfac9a3342a466f91b6e9ec53291045
Muhammad Ahsan <csahsan0021@gmail.com>
2022-01-04 15:26:43 +0500

    update the evirements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.gigaaa.link/api/v1',
   backendApi: 'https://gigaaa-backend.azurewebsites.net',
-  client_id: 12,
+  client_id: 14,
   oauth_url: "https://accounts.gconsole.io/oauth",
   accounts_url: 'https://accounts.gigaaa.com/oauth',
   redirect_uri: "https://workdeskdev.gigaaa.com/callback",
