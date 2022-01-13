322afd2c69b04f1af17bdcde60f3a0b2df59ee51
Muhammad Ahsan <csahsan0021@gmail.com>
2022-01-13 23:39:43 +0500

    workdesk update with api console endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,37 +1,37 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://api.gconsole.io/v1',
-//   backendApi: 'https://api.gconsole.io/v2',
-//   client_id: 14,
-//   oauth_url: "https://accounts.gconsole.io/oauth",
-//   accounts_url: 'https://accounts.gconsole.io/oauth',
-//   redirect_uri: "https://workdesk.gconsole.io/callback",
-//   uri: "https://workdesk.gconsole.io",
-
-//   // 
-//   prod_url_cs:"https://api.gconsole.io/cs",
-//   prod_url_workdesk:"https://api.gconsole.io/v2",
-//   prod_anlytics:"https://api.gconsole.io/analytics/",
-
-//   // 
-
-//   websocket_url:"wss://api.gconsole.io/websockets"
-// };
-
 export const environment = {
   production: true,
-  apiUrl: 'https://api.gigaaa.link/api/v1',
-  backendApi: 'https://gigaaa-backend.azurewebsites.net',
-  client_id: 12,
-  oauth_url: "https://accounts.gigaaa.com/oauth",
-  accounts_url: 'https://accounts.gigaaa.com/oauth',
-  redirect_uri: "https://workdeskdev.gigaaa.com/callback",
-  uri: "https://workdeskdev.gigaaa.com",
-  //
-  prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
-  prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
-  prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+  apiUrl: 'https://api.gconsole.io/v1',
+  backendApi: 'https://api.gconsole.io/v2',
+  client_id: 14,
+  oauth_url: "https://accounts.gconsole.io/oauth",
+  accounts_url: 'https://accounts.gconsole.io/oauth',
+  redirect_uri: "https://workdesk.gconsole.io/callback",
+  uri: "https://workdesk.gconsole.io",
 
-  //
-  websocket_url:"wss://websockets.gigaaa.com"
+  // 
+  prod_url_cs:"https://api.gconsole.io/cs",
+  prod_url_workdesk:"https://api.gconsole.io/v2",
+  prod_anlytics:"https://api.gconsole.io/analytics/",
+
+  // 
+
+  websocket_url:"wss://api.gconsole.io/websockets"
 };
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://api.gigaaa.link/api/v1',
+//   backendApi: 'https://gigaaa-backend.azurewebsites.net',
+//   client_id: 12,
+//   oauth_url: "https://accounts.gigaaa.com/oauth",
+//   accounts_url: 'https://accounts.gigaaa.com/oauth',
+//   redirect_uri: "https://workdeskdev.gigaaa.com/callback",
+//   uri: "https://workdeskdev.gigaaa.com",
+//   //
+//   prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
+//   prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
+//   prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+
+//   //
+//   websocket_url:"wss://websockets.gigaaa.com"
+// };
