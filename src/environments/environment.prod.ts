60352d998eb2a5181f4583cfa951e3ee452e3f38
Muhammad Ahsan <csahsan0021@gmail.com>
2022-01-25 02:10:09 +0500

    workdesk update for minimize video call

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,44 +1,44 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://api.gconsole.io/v1/api/v1',
-//   backendApi: 'https://api.gconsole.io/v2',
-//   client_id: 14,
-//   oauth_url: "https://accounts.gconsole.io/oauth",
-//   accounts_url: 'https://accounts.gconsole.io/oauth',
-//   redirect_uri: 'https://workdesk.gconsole.io',
-//   uri: 'https://workdesk.gconsole.io',
-
-//   accounts_callback_url: 'https://workdesk.gconsole.io',
-//   accounts_client_id: 14,
-//   //
-//   prod_url_cs:"https://api.gconsole.io/cs",
-//   prod_url_workdesk:"https://api.gconsole.io/v2",
-//   prod_anlytics:"https://api.gconsole.io/analytics",
-//   //
-//   websocket_url:"wss://api.gconsole.io/websockets",
-//     // 
-//   logged_user:'https://api.gconsole.io/v1/api/v1',
-// };
-
 export const environment = {
   production: true,
-  apiUrl: 'https://api.gigaaa.link/api/v1',
-  backendApi: 'https://gigaaa-backend.azurewebsites.net',
-  client_id: 12,
-  oauth_url: "https://accounts.gigaaa.com/oauth",
-  accounts_url: 'https://accounts.gigaaa.com/oauth',
-  redirect_uri: "https://workdeskdev.gigaaa.com/callback",
-  uri: "https://workdeskdev.gigaaa.com",
-  //
-  accounts_callback_url: 'https://workdeskdev.gigaaa.com',
-  accounts_client_id: 12,
-  //
-  prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
-  prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
-  prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+  apiUrl: 'https://api.gconsole.io/v1/api/v1',
+  backendApi: 'https://api.gconsole.io/v2',
+  client_id: 14,
+  oauth_url: "https://accounts.gconsole.io/oauth",
+  accounts_url: 'https://accounts.gconsole.io/oauth',
+  redirect_uri: 'https://workdesk.gconsole.io',
+  uri: 'https://workdesk.gconsole.io',
 
+  accounts_callback_url: 'https://workdesk.gconsole.io',
+  accounts_client_id: 14,
   //
-  websocket_url:"wss://websockets.gigaaa.com",
-     
+  prod_url_cs:"https://api.gconsole.io/cs",
+  prod_url_workdesk:"https://api.gconsole.io/v2",
+  prod_anlytics:"https://api.gconsole.io/analytics",
+  //
+  websocket_url:"wss://api.gconsole.io/websockets",
+    // 
   logged_user:'https://api.gconsole.io/v1/api/v1',
 };
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://api.gigaaa.link/api/v1',
+//   backendApi: 'https://gigaaa-backend.azurewebsites.net',
+//   client_id: 12,
+//   oauth_url: "https://accounts.gigaaa.com/oauth",
+//   accounts_url: 'https://accounts.gigaaa.com/oauth',
+//   redirect_uri: "https://workdeskdev.gigaaa.com/callback",
+//   uri: "https://workdeskdev.gigaaa.com",
+//   //
+//   accounts_callback_url: 'https://workdeskdev.gigaaa.com',
+//   accounts_client_id: 12,
+//   //
+//   prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
+//   prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
+//   prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+
+//   //
+//   websocket_url:"wss://websockets.gigaaa.com",
+     
+//   logged_user:'https://api.gconsole.io/v1/api/v1',
+// };
