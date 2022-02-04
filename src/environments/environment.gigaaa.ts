2aedef8a1a5c4eec589bc1bace5c61a7660c7b55
Muhammad Ahsan <csahsan0021@gmail.com>
2022-02-04 14:23:48 +0500

    workdesk update with environments

---
+++ src/environments/environment.gigaaa.ts
@@ -0,0 +1,21 @@
+export const environment = {
+    production: true,
+    apiUrl:'https://api.gigaaa.link/api/v1',
+    backendApi: 'https://gigaaa-backend.azurewebsites.net',
+    login:'https://api.gigaaa.link/oauth/token',
+    client_id: 8,
+    oauth_url: 'https://accounts.gigaaa.com/oauth',
+    accounts_url: 'https://accounts.gigaaa.com/oauth',
+    redirect_uri: 'http://localhost:4202/callback',
+    uri: 'http://localhost:4202',
+    accounts_callback_url: 'http://localhost:4202',
+    accounts_client_id: 8,
+    // 
+    prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
+    prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
+    prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+    //
+    websocket_url:"wss://websockets.gigaaa.com",
+
+    logged_user:'https://api.gconsole.io/v1/api/v1',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,31 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-// export const environment = {
-//     production: true,
-//     apiUrl:'https://api.gigaaa.link/api/v1',
-//     backendApi: 'https://gigaaa-backend.azurewebsites.net',
-//     login:'https://api.gigaaa.link/oauth/token',
-//     client_id: 8,
-//     oauth_url: 'https://accounts.gigaaa.com/oauth',
-//     accounts_url: 'https://accounts.gigaaa.com/oauth',
-//     redirect_uri: 'http://localhost:4202/callback',
-//     uri: 'http://localhost:4202',
-//     accounts_callback_url: 'http://localhost:4202',
-//     accounts_client_id: 8,
-//     // 
-//     prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",
-//     prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
-//     prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
-//     //
-//     websocket_url:"wss://websockets.gigaaa.com",
-// 
-//     logged_user:'https://api.gconsole.io/v1/api/v1',
-// };
 
 // for production server testing
-
 
 export const environment = {
   production: true,
