86d3713be80f7806df7bc20acbbc6726d29fc90f
Muhammad Ahsan <csahsan0021@gmail.com>
2022-03-23 19:05:53 +0500

    workdesk update for small fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -29,53 +29,53 @@
 // };
 
 
-// export const environment = {
-//   production: true,
-//   apiUrl:'https://api.gigaaa.link/api/v1',
-//   backendApi: 'https://gigaaa-backend.azurewebsites.net',
-//   login:'https://api.gigaaa.link/oauth/token',
-//   client_id: 8,
-//   oauth_url: 'https://accounts.gigaaa.com/oauth',
-//   accounts_url: 'https://accounts.gigaaa.com/oauth',
-//   redirect_uri: 'http://localhost:4202/callback',
-//   uri: 'http://localhost:4202',
-//   accounts_callback_url: 'http://localhost:4202',
-//   accounts_client_id: 8,
-//   // 
-//   prod_url_cs:"https://nlu-test.gigaaa.link",  /* https://gigaaa-customer-support.azurewebsites.net */
-//   prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
-//   prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
-//   //
-//   websocket_url:"wss://websockets.gigaaa.com",
-
-//   logged_user:'https://api.gconsole.io/v1/api/v1',
-//   currentUser: 'https://api.gigaaa.link/api/v1/current-user'
-
-
-// };
-
-// kub endpont 
 export const environment = {
   production: true,
-  apiUrl: 'https://kub-dev.gigaaa.link/accounts/users/me',
-  backendApi: 'https://kub-dev.gigaaa.link/v2',
+  apiUrl:'https://api.gigaaa.link/api/v1',
+  backendApi: 'https://gigaaa-backend.azurewebsites.net',
+  login:'https://api.gigaaa.link/oauth/token',
   client_id: 8,
-  oauth_url: "https://accounts.gigaaa.link/oauth",
-  accounts_url: 'https://accounts.gigaaa.link/oauth',
+  oauth_url: 'https://accounts.gigaaa.com/oauth',
+  accounts_url: 'https://accounts.gigaaa.com/oauth',
   redirect_uri: 'http://localhost:4202/callback',
   uri: 'http://localhost:4202',
-  //
   accounts_callback_url: 'http://localhost:4202',
   accounts_client_id: 8,
+  // 
+  prod_url_cs:"https://nlu-test.gigaaa.link",  /* https://gigaaa-customer-support.azurewebsites.net */
+  prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
+  prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
   //
-  prod_url_cs:"https://kub-dev.gigaaa.link/cs",
-  prod_url_workdesk:"https://kub-dev.gigaaa.link/v2",
-  prod_anlytics:"https://kub-dev.gigaaa.link/analytics",
-  //
-  websocket_url:"wss://kub-dev.gigaaa.link/websockets",
-    // 
-  logged_user:'https://kub-dev.gigaaa.link/v1/api/v1',
-    currentUser: 'https://kub-dev.gigaaa.link/users/me'
+  websocket_url:"wss://websockets.gigaaa.com",
+
+  logged_user:'https://api.gconsole.io/v1/api/v1',
+  currentUser: 'https://api.gigaaa.link/api/v1/current-user'
+
 
 };
+
+// kub endpont 
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://kub-dev.gigaaa.link/accounts/users/me',
+//   backendApi: 'https://kub-dev.gigaaa.link/v2',
+//   client_id: 8,
+//   oauth_url: "https://accounts.gigaaa.link/oauth",
+//   accounts_url: 'https://accounts.gigaaa.link/oauth',
+//   redirect_uri: 'http://localhost:4202/callback',
+//   uri: 'http://localhost:4202',
+//   //
+//   accounts_callback_url: 'http://localhost:4202',
+//   accounts_client_id: 8,
+//   //
+//   prod_url_cs:"https://kub-dev.gigaaa.link/cs",
+//   prod_url_workdesk:"https://kub-dev.gigaaa.link/v2",
+//   prod_anlytics:"https://kub-dev.gigaaa.link/analytics",
+//   //
+//   websocket_url:"wss://kub-dev.gigaaa.link/websockets",
+//     // 
+//   logged_user:'https://kub-dev.gigaaa.link/v1/api/v1',
+   //  currentUser: 'https://kub-dev.gigaaa.link/users/me'
+
+// };
 
