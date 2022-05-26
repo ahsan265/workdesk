7f31363be4e1b08bdcadafc37ca8da8942740eae
muhammadahsan <csahsan0021@gmail.com>
2022-05-26 17:13:41 +0500

    workdesk update for minor fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,53 +1,53 @@
 
 // for production server testing
 
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.gconsole.io/v1/api/v1',
-  backendApi: 'https://api.gconsole.io/v2',
-  client_id: 8,
-  oauth_url: "https://accounts.gconsole.io/oauth",
-  accounts_url: 'https://accounts.gconsole.io/oauth',
-  redirect_uri: 'http://localhost:4202/callback',
-  uri: 'http://localhost:4202',
-  //
-  accounts_callback_url: 'http://localhost:4202',
-  accounts_client_id: 8,
-  //
-  prod_url_cs:"https://api.gconsole.io/cs",
-  prod_url_workdesk:"https://api.gconsole.io/v2",
-  prod_anlytics:"https://api.gconsole.io/analytics",
-  //
-  websocket_url:"wss://api.gconsole.io/websockets/customer-support",
-  // 
-  logged_user:'https://api.gconsole.io/v1/api/v1',
-  currentUser: 'https://api.gconsole.io/v1/api/v1/current-user'
-
-};
-
-
 // export const environment = {
 //   production: true,
-//   apiUrl:'https://api.gigaaa.link/api/v1',
-//   backendApi: 'https://gigaaa-backend.azurewebsites.net',
-//   login:'https://api.gigaaa.link/oauth/token',
+//   apiUrl: 'https://api.gconsole.io/v1/api/v1',
+//   backendApi: 'https://api.gconsole.io/v2',
 //   client_id: 8,
-//   oauth_url: 'https://accounts.gigaaa.com/oauth',
-//   accounts_url: 'https://accounts.gigaaa.com/oauth',
+//   oauth_url: "https://accounts.gconsole.io/oauth",
+//   accounts_url: 'https://accounts.gconsole.io/oauth',
 //   redirect_uri: 'http://localhost:4202/callback',
 //   uri: 'http://localhost:4202',
+//   //
 //   accounts_callback_url: 'http://localhost:4202',
 //   accounts_client_id: 8,
-//   // 
-//   prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",  /* https://gigaaa-customer-support.azurewebsites.net */
-//   prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
-//   prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
 //   //
-//   websocket_url:"wss://kub-dev.gigaaa.link/websockets/cs", /*  wss://kub-dev.gigaaa.link/websockets */
-
+//   prod_url_cs:"https://api.gconsole.io/cs",
+//   prod_url_workdesk:"https://api.gconsole.io/v2",
+//   prod_anlytics:"https://api.gconsole.io/analytics",
+//   //
+//   websocket_url:"wss://api.gconsole.io/websockets/customer-support",
+//   // 
 //   logged_user:'https://api.gconsole.io/v1/api/v1',
-//   currentUser: 'https://api.gigaaa.link/api/v1/current-user'
+//   currentUser: 'https://api.gconsole.io/v1/api/v1/current-user'
+
 // };
+
+
+export const environment = {
+  production: true,
+  apiUrl:'https://api.gigaaa.link/api/v1',
+  backendApi: 'https://gigaaa-backend.azurewebsites.net',
+  login:'https://api.gigaaa.link/oauth/token',
+  client_id: 8,
+  oauth_url: 'https://accounts.gigaaa.com/oauth',
+  accounts_url: 'https://accounts.gigaaa.com/oauth',
+  redirect_uri: 'http://localhost:4202/callback',
+  uri: 'http://localhost:4202',
+  accounts_callback_url: 'http://localhost:4202',
+  accounts_client_id: 8,
+  // 
+  prod_url_cs:"https://gigaaa-customer-support.azurewebsites.net",  /* https://gigaaa-customer-support.azurewebsites.net */
+  prod_url_workdesk:"https://gigaaa-backend.azurewebsites.net",
+  prod_anlytics:"https://gigaaa-analytics.azurewebsites.net",
+  //
+  websocket_url:"wss://kub-dev.gigaaa.link/websockets/cs", /*  wss://kub-dev.gigaaa.link/websockets */
+
+  logged_user:'https://api.gconsole.io/v1/api/v1',
+  currentUser: 'https://api.gigaaa.link/api/v1/current-user'
+};
 
 // kub endpont 
 // export const environment = {
