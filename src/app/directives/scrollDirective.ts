40092ccd28cbaa1731238821f205adc97c54c0dd
@muhammadahsan <csahsan0021@gmail.com>
2022-11-05 02:50:38 +0500

    workdesk fixes

--- src/app/directives/scrollDirective.ts
+++ src/app/directives/scrollDirective.ts
@@ -5,13 +5,16 @@ import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 })
 export class scrollStageDirective {
   @HostBinding('style.box-shadow') private beforeScroll = 'none';
-  constructor() {
-  }
+  constructor() {}
   @HostListener('document:wheel', ['$event']) private onScroll(event: any) {
     const element = document.querySelector('.inPageWrapper') as HTMLElement;
     const element1 = document.querySelector('.filterWrapper') as HTMLElement;
-    console.log(element.getBoundingClientRect().top, element1.getBoundingClientRect().bottom + 2);
-    (element.getBoundingClientRect().top === element1.getBoundingClientRect().bottom + 2)
+    console.log(
+      element.getBoundingClientRect().top,
+      element1.getBoundingClientRect().bottom + 2
+    );
+    element.getBoundingClientRect().top ===
+    element1.getBoundingClientRect().bottom + 2
       ? (this.beforeScroll = 'none')
       : (this.beforeScroll = '2px 3px 4px 0px rgb(0 0 0 / 25%)');
   }
